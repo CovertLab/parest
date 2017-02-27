2dfc82865196530e6d8a93cb83074b41828247e6
John Mason <jmason42@stanford.edu>
2017-02-27 14:56:33 -0800

    minor optimization

--- utils/bounds.py
+++ utils/bounds.py
@@ -4,14 +4,16 @@ import numpy as np
 def unbounded(x, upperbounds, lowerbounds):
 	return (x > upperbounds) | (x < lowerbounds)
 
-def unbounded_to_random(x, upperbounds, lowerbounds):
+# this is a good candidate for cythonization
+def unbounded_to_random(x, upperbounds, lowerbounds, random_state = np.random):
 	ub = unbounded(x, upperbounds, lowerbounds)
 
-	x = x.copy()
+	if ub.any():
+		x = x.copy()
 
-	x[ub] = (
-		lowerbounds
-		+ np.random.random(size = x.shape) * (upperbounds - lowerbounds)
-		)[ub]
+		x[ub] = (
+			lowerbounds
+			+ random_state.random_sample(size = x.shape) * (upperbounds - lowerbounds)
+			)[ub]
 
 	return x
