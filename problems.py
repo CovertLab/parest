55b196203806062a45f80a33a93bb4cec0e30f71
John Mason <jmason42@stanford.edu>
2017-04-25 14:44:10 -0700

    misc new problems

--- problems.py
+++ problems.py
@@ -8,7 +8,7 @@ DEFINITIONS = OrderedDict()
 
 DEFINITIONS['data_agnostic'] = tuple()
 
-DEFINITIONS['no_data'] = ((lambda entry: True, 0),)
+# DEFINITIONS['no_data'] = ((lambda entry: True, 0),)
 
 def exclude_entry(excluded_entry):
 	return (
@@ -27,6 +27,124 @@ for entry in kb.standard_energy_of_formation:
 		problem_name = 'exclude_{}'.format(entry.id).replace(':', '_')
 
 		DEFINITIONS[problem_name] = exclude_entry(entry)
+
+import fitting
+
+_KINETICS_TYPES = (
+	'forward_catalytic_rate',
+	'reverse_catalytic_rate',
+	'substrate_saturation'
+	)
+
+DEFINITIONS['custom'] = (
+	(
+		fitting.field_value_rule(
+			datatype = ('concentration', 'standard_energy_of_formation'),
+			compound = ('H2O', 'Pi', 'H')
+			),
+		1e1
+		),
+	(
+		fitting.field_value_rule(
+			datatype = ('concentration', 'relative_protein_count'),
+			),
+		1e0
+		),
+	(
+		fitting.field_value_rule(
+			datatype = _KINETICS_TYPES
+			),
+		1e-1
+		),
+	(
+		fitting.field_value_rule(
+			datatype = ('standard_energy_of_formation',),
+			),
+		1e-2
+		),
+	)
+
+DEFINITIONS['custom2'] = (
+	(
+		fitting.field_value_rule(
+			datatype = ('concentration', 'standard_energy_of_formation'),
+			compound = ('H2O', 'Pi', 'H')
+			),
+		1e1
+		),
+	(
+		fitting.field_value_rule(
+			datatype = _KINETICS_TYPES
+			),
+		1e0
+		),
+	(
+		fitting.field_value_rule(
+			datatype = ('concentration', 'relative_protein_count'),
+			),
+		1e-1
+		),
+	(
+		fitting.field_value_rule(
+			datatype = ('standard_energy_of_formation',),
+			),
+		1e-2
+		),
+	)
+
+DEFINITIONS['no_kinetics'] = (
+	(
+		fitting.field_value_rule(
+			datatype = _KINETICS_TYPES,
+			),
+		0
+		),
+	(
+		lambda entry: True,
+		1e0
+		)
+	)
+
+DEFINITIONS['no_proteomics'] = (
+	(
+		fitting.field_value_rule(
+			datatype = ('relative_protein_count'),
+			),
+		0
+		),
+	(
+		lambda entry: True,
+		1e0
+		)
+	)
+
+DEFINITIONS['promote_fba_kinetics'] = (
+	(
+		fitting.field_value_rule(
+			datatype = _KINETICS_TYPES,
+			reaction = 'FBA'
+			),
+		1e1
+		),
+	(
+		lambda entry: True,
+		1e0
+		)
+	)
+
+DEFINITIONS['promote_fba_kinetics2'] = (
+	(
+		fitting.field_value_rule(
+			datatype = _KINETICS_TYPES,
+			reaction = 'FBA'
+			),
+		1e2
+		),
+	(
+		lambda entry: True,
+		1e0
+		)
+	)
 
 def test():
 	print '{} problem definitions:'.format(len(DEFINITIONS))
