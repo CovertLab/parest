b362b4a6b07b3f3695eada6bcfe2cb1f11d24197
John Mason <jmason42@stanford.edu>
2018-02-27 15:17:10 -0800

    removed several outdated problem definitions

--- problems.py
+++ problems.py
@@ -5,16 +5,15 @@ from data import kb
 import structure
 import fitting
 
-_DEFAULT_EXCLUDE = dict(
-	datatype = (
-		'custom_saturation_limits',
-		'standard_energy_of_formation'
-		)
+_DEFAULT_EXCLUDE_DATATYPE = (
+	'upper_reactant_saturation_limit',
+	'upper_product_saturation_limit',
+	'standard_energy_of_formation'
 	)
 
 _DEFAULT_RULES = (
 	(
-		fitting.field_value_rule(**_DEFAULT_EXCLUDE),
+		fitting.field_value_rule(datatype = _DEFAULT_EXCLUDE_DATATYPE),
 		0
 		),
 	)
@@ -37,165 +36,100 @@ DEFINITIONS['data_agnostic'] = _DEFAULT_RULES + (accept_all(),)
 
 DEFINITIONS['no_data'] = _DEFAULT_RULES + (accept_all(0),)
 
-for entry in kb.concentration:
-	if entry.compound in structure.compounds:
-		problem_name = 'exclude_{}'.format(entry.id).replace(':', '_')
-
-		DEFINITIONS[problem_name] = _DEFAULT_RULES + exclude_entry(entry)
-
-for entry in kb.standard_energy_of_formation:
-	if entry.compound in structure.compounds:
-		problem_name = 'exclude_{}'.format(entry.id).replace(':', '_')
-
-		DEFINITIONS[problem_name] = _DEFAULT_RULES + exclude_entry(entry)
-
 _KINETICS_TYPES = (
 	'forward_catalytic_rate',
 	'reverse_catalytic_rate',
 	'substrate_saturation'
 	)
 
-DEFINITIONS['custom'] = _DEFAULT_RULES + (
+from collections import defaultdict
+
+def gather_by_fields(dataset, *fields):
+	out = defaultdict(set)
+
+	for entry in dataset:
+		out[tuple(getattr(entry, field) for field in fields)].add(entry)
+
+	return out
+
+def gather_by_field(dataset, field):
+	return {
+		key[0]:value
+		for key, value in gather_by_fields(dataset, field).viewitems()
+		}
+
+from itertools import izip
+
+def scale_observations(dataset, *fields):
+	return tuple(
+		(
+			fitting.field_value_rule(**{
+				field:(value,)
+				for (field, value) in izip(fields, values)
+				}),
+			1.0/len(entries)
+			)
+		for (values, entries) in gather_by_fields(
+			dataset,
+			*fields
+			).viewitems()
+		)
+
+DEFINITIONS['all_scaled'] = (
+	_DEFAULT_RULES
+	+ scale_observations(kb.concentration, 'datatype', 'compound')
+	+ scale_observations(kb.standard_energy_of_formation, 'datatype', 'compound')
+	+ scale_observations(kb.equilibrium, 'datatype', 'reaction')
+	+ scale_observations(kb.forward_catalytic_rate, 'datatype', 'reaction')
+	+ scale_observations(kb.reverse_catalytic_rate, 'datatype', 'reaction')
+	+ scale_observations(kb.reactant_saturation, 'datatype', 'reaction', 'compound')
+	+ scale_observations(kb.product_saturation, 'datatype', 'reaction', 'compound')
+	) + (
 	(
-		fitting.field_value_rule(
-			datatype = ('concentration', 'standard_energy_of_formation'),
-			compound = ('H2O', 'Pi', 'H', 'ATP', 'AMP', 'ADP')
+		fitting.field_value_rule( # relative data hard to scale by # of obs; instead scaling by total # of data sets (3)
+			datatype = ('relative_protein_count',),
 			),
-		1e1
+		1.0/3
 		),
+	)
+
+DEFINITIONS['all_scaled_upper_sat_limits_1e-1'] = (
 	(
-		fitting.field_value_rule(datatype = ('standard_energy_of_formation',)),
+		fitting.field_value_rule(source = ('custom_saturation_limits',)),
 		1e-1
 		),
-	accept_all()
-	)
+	) + DEFINITIONS['all_scaled']
 
-DEFINITIONS['custom2'] = _DEFAULT_RULES + (
+DEFINITIONS['all_scaled_upper_sat_limits_1e0'] = (
 	(
-		fitting.field_value_rule(
-			datatype = ('concentration', 'standard_energy_of_formation'),
-			compound = ('H2O', 'Pi')
-			),
+		fitting.field_value_rule(source = ('custom_saturation_limits',)),
+		1e0
+		),
+	) + DEFINITIONS['all_scaled']
+
+DEFINITIONS['all_scaled_upper_sat_limits_1e1'] = (
+	(
+		fitting.field_value_rule(source = ('custom_saturation_limits',)),
 		1e1
 		),
-	accept_all()
-	)
+	) + DEFINITIONS['all_scaled']
 
-DEFINITIONS['no_seof'] = _DEFAULT_RULES + (
+DEFINITIONS['all_scaled_upper_sat_limits_1e2'] = (
 	(
-		fitting.field_value_rule(datatype = ('standard_energy_of_formation',)),
-		0
-		),
-	accept_all()
-	)
-
-DEFINITIONS['seof_1e-1'] = _DEFAULT_RULES + (
-	(
-		fitting.field_value_rule(datatype = ('standard_energy_of_formation',)),
-		1e-1
-		),
-	accept_all()
-	)
-
-DEFINITIONS['no_seof_PEP_conc_100x'] = _DEFAULT_RULES + (
-	(
-		fitting.field_value_rule(datatype = ('standard_energy_of_formation',)),
-		0
-		),
-	(
-		fitting.field_value_rule(
-			datatype = ('concentration',),
-			compound = ('PEP',)
-			),
+		fitting.field_value_rule(source = ('custom_saturation_limits',)),
 		1e2
 		),
-	accept_all()
-	)
-
-DEFINITIONS['no_seof_gap_kinetics_100x'] = _DEFAULT_RULES + (
-	(
-		fitting.field_value_rule(datatype = ('standard_energy_of_formation',)),
-		0
-		),
-	(
-		fitting.field_value_rule(
-			datatype = _KINETICS_TYPES,
-			reaction = 'GAP'
-			),
-		1e2
-		),
-	accept_all()
-	)
-
-DEFINITIONS['no_seof_gap_kinetics_100x_scaled'] = _DEFAULT_RULES + (
-	(
-		fitting.field_value_rule(datatype = ('standard_energy_of_formation',)),
-		0
-		),
-	(
-		fitting.field_value_rule(
-			datatype = _KINETICS_TYPES,
-			reaction = 'GAP'
-			),
-		1e1
-		),
-	accept_all(1e-1)
-	)
-
-DEFINITIONS['upper_sat_limits'] = tuple()
-
-DEFINITIONS['upper_sat_limits_x10'] = (
-	(lambda entry: entry.source == 'custom_saturation_limits', 10.0),
-	(lambda entry: True, 1.0),
-	)
-
-DEFINITIONS['Keq_e-1'] = _DEFAULT_RULES + (
-	(
-		lambda entry: entry.datatype == 'equilibrium',
-		1e-1
-		),
-	accept_all()
-	)
-
-DEFINITIONS['Keq_half'] = _DEFAULT_RULES + (
-	(
-		lambda entry: entry.datatype == 'equilibrium',
-		1e-1
-		),
-	accept_all()
-	)
-
-DEFINITIONS['Keq_FBA_TPI_e-1'] = _DEFAULT_RULES + (
-	(
-		fitting.field_value_rule(
-			datatype = ('equilibrium'),
-			reaction = ('FBA', 'TPI', 'FBA_TPI')
-			),
-		1e-1
-		),
-	accept_all()
-	)
-
-DEFINITIONS['Keq_FBA_TPI_ENO_e-1'] = _DEFAULT_RULES + (
-	(
-		fitting.field_value_rule(
-			datatype = ('equilibrium'),
-			reaction = ('FBA', 'TPI', 'FBA_TPI', 'ENO')
-			),
-		1e-1
-		),
-	accept_all()
-	)
+	) + DEFINITIONS['all_scaled']
 
 def test():
 	print '{} problem definitions:'.format(len(DEFINITIONS))
 	print '\n'.join(DEFINITIONS.viewkeys())
 
-	from fitting import build_fitting_tensors
+	import fitting
 
 	for name, rules_and_weights in DEFINITIONS.viewitems():
-		build_fitting_tensors(*rules_and_weights)
-		build_relative_fitting_tensor_sets(*rules_and_weights)
+		fitting.build_fitting_tensors(*rules_and_weights)
+		fitting.build_relative_fitting_tensor_sets(*rules_and_weights)
 
 if __name__ == '__main__':
 	test()
