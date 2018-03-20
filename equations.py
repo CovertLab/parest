270f3857304b7097a21ca316b562796f7e4888fe
John Mason <jmason42@stanford.edu>
2018-03-20 11:52:16 -0700

    moved theano-jacobian function and added a note about usage

--- equations.py
+++ equations.py
@@ -1,78 +1,6 @@
 
 from __future__ import division
 
-def build_jacobian():
-	import theano as th
-	import theano.tensor as tn
-
-	gibbs_energies = tn.dvector('Molar Gibbs energies')
-
-	mu = tn.dscalar('Growth rate')
-
-	k_star = tn.dscalar('Fundamental catalytic rate constant')
-	RT = tn.dscalar('RT')
-
-	forward_reaction_potential = tn.dmatrix('Forward reaction potential matrix')
-	reverse_reaction_potential = tn.dmatrix('Reverse reaction potential matrix')
-
-	forward_binding_potential = tn.dmatrix('Forward binding potential matrix')
-	reverse_binding_potential = tn.dmatrix('Reverse binding potential matrix')
-
-	reaction_forward_binding_association = tn.dmatrix('Reaction forward binding association matrix')
-	reaction_reverse_binding_association = tn.dmatrix('Reaction reverse binding association matrix')
-
-	stoich = tn.dmatrix('Stoichiometry matrix')
-
-	glc_association = tn.dmatrix('Molar Gibbs energies from log-concentration association matrix')
-
-	frp = forward_reaction_potential.dot(gibbs_energies)
-	rrp = reverse_reaction_potential.dot(gibbs_energies)
-
-	fbp = forward_binding_potential.dot(gibbs_energies)
-	rbp = reverse_binding_potential.dot(gibbs_energies)
-
-	denom = (
-		1
-		+ reaction_forward_binding_association.dot(tn.exp(-fbp/RT))
-		+ reaction_reverse_binding_association.dot(tn.exp(-rbp/RT))
-		)
-
-	v = k_star * (tn.exp(-frp/RT) - tn.exp(-rrp/RT)) / denom
-
-	glc = glc_association.dot(gibbs_energies)
-
-	inputs = (
-		gibbs_energies,
-
-		mu,
-
-		k_star,
-		RT,
-
-		forward_reaction_potential,
-		reverse_reaction_potential,
-
-		forward_binding_potential,
-		reverse_binding_potential,
-
-		reaction_forward_binding_association,
-		reaction_reverse_binding_association,
-
-		stoich,
-
-		glc_association,
-		)
-
-	dglc_dt = RT * tn.exp(-glc/RT) * stoich.dot(v) - RT * mu
-
-	jac_dglc_dt = tn.jacobian(dglc_dt, gibbs_energies).dot(
-		glc_association.T
-		)
-
-	f_jac_dglc_dt = th.function(inputs, jac_dglc_dt)
-
-	return f_jac_dglc_dt
-
 def build_equations_uncompiled():
 	import numpy as np
 
@@ -323,6 +251,85 @@ def reverse_reaction_rates(
 		)
 
 	return k_star * np.exp(-rrp/RT) / denom
+
+def build_jacobian():
+	'''
+	This function uses Theano, a symbolic math library, to analytically
+	evaluate the Jacobian of the dynamical system.  In the spirit of minimizing
+	dependencies, and due to some performance issues, I opted to not use Theano
+	for this work.  However I retained this function for legacy purposes.
+	'''
+
+	import theano as th
+	import theano.tensor as tn
+
+	gibbs_energies = tn.dvector('Molar Gibbs energies')
+
+	mu = tn.dscalar('Growth rate')
+
+	k_star = tn.dscalar('Fundamental catalytic rate constant')
+	RT = tn.dscalar('RT')
+
+	forward_reaction_potential = tn.dmatrix('Forward reaction potential matrix')
+	reverse_reaction_potential = tn.dmatrix('Reverse reaction potential matrix')
+
+	forward_binding_potential = tn.dmatrix('Forward binding potential matrix')
+	reverse_binding_potential = tn.dmatrix('Reverse binding potential matrix')
+
+	reaction_forward_binding_association = tn.dmatrix('Reaction forward binding association matrix')
+	reaction_reverse_binding_association = tn.dmatrix('Reaction reverse binding association matrix')
+
+	stoich = tn.dmatrix('Stoichiometry matrix')
+
+	glc_association = tn.dmatrix('Molar Gibbs energies from log-concentration association matrix')
+
+	frp = forward_reaction_potential.dot(gibbs_energies)
+	rrp = reverse_reaction_potential.dot(gibbs_energies)
+
+	fbp = forward_binding_potential.dot(gibbs_energies)
+	rbp = reverse_binding_potential.dot(gibbs_energies)
+
+	denom = (
+		1
+		+ reaction_forward_binding_association.dot(tn.exp(-fbp/RT))
+		+ reaction_reverse_binding_association.dot(tn.exp(-rbp/RT))
+		)
+
+	v = k_star * (tn.exp(-frp/RT) - tn.exp(-rrp/RT)) / denom
+
+	glc = glc_association.dot(gibbs_energies)
+
+	inputs = (
+		gibbs_energies,
+
+		mu,
+
+		k_star,
+		RT,
+
+		forward_reaction_potential,
+		reverse_reaction_potential,
+
+		forward_binding_potential,
+		reverse_binding_potential,
+
+		reaction_forward_binding_association,
+		reaction_reverse_binding_association,
+
+		stoich,
+
+		glc_association,
+		)
+
+	dglc_dt = RT * tn.exp(-glc/RT) * stoich.dot(v) - RT * mu
+
+	jac_dglc_dt = tn.jacobian(dglc_dt, gibbs_energies).dot(
+		glc_association.T
+		)
+
+	f_jac_dglc_dt = th.function(inputs, jac_dglc_dt)
+
+	return f_jac_dglc_dt
 
 import constants
 import structure
