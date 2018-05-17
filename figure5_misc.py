549901ca0edbf5b08f918d0ec2f92b227d57c082
John Mason <jmason42@stanford.edu>
2018-05-17 14:28:21 -0700

    added new problem type

--- figure5_misc.py
+++ figure5_misc.py
@@ -3,8 +3,18 @@ import os
 
 import figure5
 
-for penalty in ('1e-1', '1e0', '1e1', '1e2'):
-	figure5.main(
+inputs_and_outputs = tuple(
+	(
 		os.path.join('out', 'all_scaled_upper_sat_limits_{}'.format(penalty)),
 		os.path.join('figure5', 'saturation penalized', 'penalty_{}'.format(penalty))
 		)
+	for penalty in ('1e-1', '1e0', '1e1', '1e2')
+	) + (
+	(
+		os.path.join('out', 'all_scaled_Pi_H2O_1e1'),
+		os.path.join('figure5', 'all_scaled_Pi_H2O_1e1')
+		),
+	)
+
+for input_and_output in inputs_and_outputs:
+	figure5.main(*input_and_output)
--- figure6_misc.py
+++ figure6_misc.py
@@ -3,8 +3,19 @@ import os
 
 import figure6
 
-for penalty in ('1e-1', '1e0', '1e1', '1e2'):
-	figure6.main(
+inputs_and_outputs = tuple(
+	(
 		os.path.join('out', 'all_scaled_upper_sat_limits_{}'.format(penalty)),
 		os.path.join('figure6', 'saturation penalized', 'penalty_{}'.format(penalty))
 		)
+	for penalty in ('1e-1', '1e0', '1e1', '1e2')
+	) + (
+	(
+		os.path.join('out', 'all_scaled_Pi_H2O_1e1'),
+		os.path.join('figure6', 'all_scaled_Pi_H2O_1e1')
+		),
+	)
+
+for input_and_output in inputs_and_outputs:
+	figure6.main(*input_and_output)
+
